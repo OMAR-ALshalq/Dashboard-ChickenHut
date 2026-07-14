166907062df07e4a70f05deaffbeb34869729d2c
OMAR-ALshalq <omaralshalq@gmail.com>
2026-07-14 17:43:47 +0300

    edit Dashboard

---
+++ public/service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "chickenhut-admin-v1";
+const urlsToCache = ["/", "/index.html", "/manifest.json", "/Logo.jpg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
